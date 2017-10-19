ba290f588a5c21120035cc916ad1c425e7ace943
david2806 <32930439+david2806@users.noreply.github.com>
2017-10-19 07:21:20 -0500

    Add files via upload

---
+++ productoMD.php
@@ -0,0 +1,46 @@
+<?php
+        
+    class productoMD
+    {
+        var $codigo;
+        var $tipo;
+		var $nombre;
+        var $precio;
+				
+        function setCodigo($codigo)
+		{
+            $this->codigo=$codigo;
+        }
+		
+		function setTipo($tipo) 
+        {
+            $this->tipo = $tipo;
+        }
+		
+		function setNombre($nombre)
+		{
+            $this->nombre=$nombre;
+        }
+         
+		function setPrecio($precio) 
+        {
+            $this->precio = $precio;
+        }
+		
+		function getCodigo() {
+			return $this->codigo;
+        }
+		
+		function getTipo() {
+			return $this->tipo;
+        }
+		
+        function getNombre() {
+			return $this->nombre;
+        }
+        
+		function getPrecio() {
+			return $this->precio;
+        }
+    }
+ ?>
