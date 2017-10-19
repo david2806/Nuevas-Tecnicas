aa0883ec56a84c60e38b2a2662c809393086f63d
Vrfm <32935626+Vrfm@users.noreply.github.com>
2017-10-19 11:06:54 -0500

    Add files via upload

---
+++ hichaMD.php
@@ -0,0 +1,33 @@
+<?php
+        
+    class hinchaMD
+    {
+        var $nombre;
+        var $genero;
+        var $tipoPersona;
+		
+        function setNombre($nombre)
+		{
+            $this->nombre=$nombre;
+        }
+        function setGenero($genero) 
+        {
+            $this->genero = $genero;
+        }
+		function setTipoPersona($tipoPersona) 
+        {
+            $this->tipoPersona = $tipoPersona;
+        }
+                    
+        function getNombre() {
+			return $this->nombre;
+        }
+
+        function getGenero() {
+        return $this->genero;
+        }
+		function getTipoPersona() {
+			return $this->tipoPersona;
+        }
+    }
+ ?>
