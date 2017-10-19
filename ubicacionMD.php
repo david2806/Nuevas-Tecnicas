07c85b72ff1912a4180af28cdc5588e4c78ab5e1
jeanpierre88 <32936467+jeanpierre88@users.noreply.github.com>
2017-10-19 11:06:53 -0500

    Add files via upload

---
+++ ubicacionMD.php
@@ -0,0 +1,43 @@
+<?php
+        
+    class ubicacionMD
+    {
+        var $zona;
+		var $bloque;
+        var $fila;
+        var $puesto;
+		
+        function setZona($zona)
+		{
+            $this->zona=$zona;
+        }
+        function setBloque($bloque) 
+        {
+            $this->bloque = $bloque;
+        }
+		function setFila($fila) 
+        {
+            $this->fila = $fila;
+        }
+		function setPuesto($puesto) 
+        {
+            $this->puesto = $puesto;
+        }
+                    
+        function getZona() {
+			return $this->zona;
+        }
+
+        function getBloque() {
+			return $this->bloque;
+        }
+		
+		function getFila() {
+			return $this->fila;
+        }
+		
+		function getPuesto() {
+			return $this->puesto;
+        }
+    }
+ ?>
